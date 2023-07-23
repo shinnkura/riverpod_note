c7de3d2fae8bdbac7d775b3b4b53fd06e893aad0
shinnkura <n.shinzo1010@icloud.com>
2023-07-24 00:20:17 +0900

    basic_provider_casheディレクトリを作成

---
+++ lib/basic_provider_cache/state.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'state.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$scoreNotifierHash() => r'2a3613fe7be14039a69511a8498d7fc36b674d17';
+
+/// See also [ScoreNotifier].
+@ProviderFor(ScoreNotifier)
+final scoreNotifierProvider =
+    NotifierProvider<ScoreNotifier, List<int>>.internal(
+  ScoreNotifier.new,
+  name: r'scoreNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$scoreNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ScoreNotifier = Notifier<List<int>>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
