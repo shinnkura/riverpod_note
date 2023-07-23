3e515623c903aa1a20ffe148491324045d2fb24f
shinnkura <n.shinzo1010@icloud.com>
2023-07-24 00:25:44 +0900

    basic_provider_loディレクトリ作成

---
+++ lib/basic_provider_logic.dart/state.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'state.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$ageNotifierHash() => r'296c6c51ab9f7d06a71350cb6a2d81da97b8beda';
+
+/// See also [AgeNotifier].
+@ProviderFor(AgeNotifier)
+final ageNotifierProvider =
+    AutoDisposeNotifierProvider<AgeNotifier, int>.internal(
+  AgeNotifier.new,
+  name: r'ageNotifierProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$ageNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$AgeNotifier = AutoDisposeNotifier<int>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
