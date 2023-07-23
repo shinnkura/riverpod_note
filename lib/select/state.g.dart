7f9a815f673c4cc330c45a7735d851cec6e72354
shinnkura <n.shinzo1010@icloud.com>
2023-07-24 08:45:42 +0900

    selectディレクトリを作成

---
+++ lib/select/state.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'state.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$shikokuNotifierHash() => r'a66b2696dadca7d0a82afa5612db6f7b9a79b6a6';
+
+/// See also [ShikokuNotifier].
+@ProviderFor(ShikokuNotifier)
+final shikokuNotifierProvider =
+    AutoDisposeNotifierProvider<ShikokuNotifier, dynamic>.internal(
+  ShikokuNotifier.new,
+  name: r'shikokuNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$shikokuNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$ShikokuNotifier = AutoDisposeNotifier<dynamic>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
