c558b55f7b29de0085305398b5f50b794b5c1fa5
shinnkura <n.shinzo1010@icloud.com>
2023-07-24 00:35:25 +0900

    build_ondisposeディレクトリを作成

---
+++ lib/build_ondispose/state.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'state.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$countNotifierHash() => r'b8b8bd1bbc162f7117603b9ef3c3fb8c7efeaee9';
+
+/// See also [CountNotifier].
+@ProviderFor(CountNotifier)
+final countNotifierProvider =
+    AutoDisposeNotifierProvider<CountNotifier, int>.internal(
+  CountNotifier.new,
+  name: r'countNotifierProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$countNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$CountNotifier = AutoDisposeNotifier<int>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
