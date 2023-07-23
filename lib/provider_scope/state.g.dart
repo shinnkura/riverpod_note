ec078d78359bad27002fc4d76b1115504d05665b
shinnkura <n.shinzo1010@icloud.com>
2023-07-24 08:42:20 +0900

    provider_scopeディレクトリを作成

---
+++ lib/provider_scope/state.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'state.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$testNotifierHash() => r'11ed7357e11e837ddec3df733c16754b0d81dc1e';
+
+/// See also [TestNotifier].
+@ProviderFor(TestNotifier)
+final testNotifierProvider =
+    AutoDisposeNotifierProvider<TestNotifier, int>.internal(
+  TestNotifier.new,
+  name: r'testNotifierProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$testNotifierHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$TestNotifier = AutoDisposeNotifier<int>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
