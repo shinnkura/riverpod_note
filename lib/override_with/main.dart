c92236b9fe906151baf8e02b37f870ff7a1bddf1
shinnkura <n.shinzo1010@icloud.com>
2023-07-24 08:33:59 +0900

    override_withディレクトリを作成

---
+++ lib/override_with/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'state.dart';
+import 'widget.dart';
+
+void main() {
+  const app = MyApp();
+
+  final scope = ProviderScope(
+    overrides: [
+      // ここに偽物のデータを使いたいプロバイダーたち
+
+      appleProvider.overrideWith((ref) {
+        return '毒りんご';
+      }),
+    ],
+    child: app,
+  );
+  runApp(scope);
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: MyWidget(),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/override_with/state.g.dart
@@ -0,0 +1,23 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'state.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$appleHash() => r'6e4f7de9ec95863045593403521e4d0882cc2fbc';
+
+/// See also [apple].
+@ProviderFor(apple)
+final appleProvider = AutoDisposeProvider<String>.internal(
+  apple,
+  name: r'appleProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$appleHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef AppleRef = AutoDisposeProviderRef<String>;
+// ignore_for_file: unnecessary_raw_strings, subtype_of_sealed_class, invalid_use_of_internal_member, do_not_use_environment, prefer_const_constructors, public_member_api_docs, avoid_private_typedef_functions
